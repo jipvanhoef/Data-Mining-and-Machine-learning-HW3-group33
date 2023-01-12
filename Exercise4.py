41ffe5b2625b4a363a5b1774ccc0b26a9946cdbb
jipvanhoef <jipvanhoef@gmail.com>
2023-01-12 15:16:42 +0100

    added more of ex 4, some problems with matrix math

--- Exercise4.py
+++ Exercise4.py
@@ -8,8 +8,13 @@ from sklearn.decomposition import TruncatedSVD
 np.random.seed()
 
 # lets explore movies.csv
-movies= pd.read_csv('ml-latest-small/movies.csv')
-ratings=pd.read_csv('ml-latest-small/ratings.csv',sep=',')
+movies= pd.read_csv('data/movies.csv')
+ratings=pd.read_csv('data/ratings.csv',sep=',')
+
+#init values
+tmax = 100
+r = 5
+labda = 0.0001
 
 #convert sparse representation to data matrix
 df_movie_ratings = ratings.pivot(
@@ -33,6 +38,38 @@ D = df_D.to_numpy()
 def InitRandom(n, d, r ):
     X = np.random.rand(d, r)
     Y = np.random.rand(n, r)
+    return X, Y
 
 def IndicatorNonzero(D):
-    
+    return ( D != 0).astype(int)
+
+def matrix_completion(D, r, tmax, labda):
+    d, n = D.shape
+    X, Y = InitRandom(n, d, r)
+    O = IndicatorNonzero(D)
+    t = 1
+    while t < tmax:
+        for k in range(d):
+            OXk = np.diag(O[:,k])
+            X[:,k] = D.T.dot(k*Y) @ np.linalg.inv(Y.T @ OXk @ Y + labda*np.eye(r))
+            for i in range(n):
+                OYi = np.diag(O[i,:])
+                Y[i,:] = D[i]*X @ np.linalg.inv(X.T @ OYi @ X + labda*np.eye(r))
+            t += 1
+    return X, Y
+
+# Run matrix completion algorithm
+X, Y = matrix_completion(D, r, tmax, labda)
+
+# Calculate MSEO at each iteration
+O = IndicatorNonzero(D)
+mses = []
+for t in range(1, tmax+1):
+    mse = 1/np.sum(O) * np.linalg.norm(D - O*(Y @ X.T))**2
+    mses.append(mse)
+
+# Plot MSEO vs. iteration
+plt.plot(range(1, tmax+1), mses)
+plt.xlabel('Iteration')
+plt.ylabel('MSEO')
+plt.show()
